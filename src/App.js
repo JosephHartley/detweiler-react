aa3f015f64354f78e26e6c42bcd406bad7b73a60
Joe Hartley <joejohnhartley@gmail.com>
2023-11-10 18:14:52 +0000

    adds manual sitemap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import Router from "./Router";
+
+import Home from "./views/Home";
+import Gigs from "./views/Gigs";
+import News from "./views/News";
+import Releases from "./views/Releases";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -13,7 +18,12 @@ function App() {
         <div className="sticky top-0 max-w-full backdrop-opacity-10 backdrop-invert bg-blue-300/10 shadow-lg z-10">
           <Header />
         </div>
-          <Router />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gigs" element={<Gigs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/releases" element={<Releases />} />
+          </Routes>
         <div className="md:sticky bottom-0 max-w-full backdrop-opacity-10 backdrop-invert bg-blue-300/10 shadow-lg z-10">
           <Footer />
         </div>
--- src/sitemap.js
+++
@@ -1,8 +0,0 @@
-import { generateSitemap } from 'react-sitemap-generator';
-import Router from './Router';
-
-generateSitemap({
-  url: 'https://www.detweiler.co.uk',
-  routes: [Router],
-  output: './public',
-});
